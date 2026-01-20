fe5d6078cda2e3be8e7bb7bbe8db309324fad3ad
HeitorRollemberg <heitor.rollemberg@souunit.com.br>
2026-01-20 19:55:46 -0300

    combatente atualizado dando e recebendo dano

--- Combatente.java
+++ Combatente.java
@@ -1,31 +1,54 @@
-public class Combatente {
-    private String nome;
-    private int experiencia;
-    private int vida;
+import java.util.Random; 
 
+public class Combatente {
+    // --- ATRIBUTOS ---
+    private String nome;
+    private int vida;
+    private int experiencia;
+
+    // --- CONSTRUTOR ---
     public Combatente(String nome, int vidaInicial) {
         this.nome = nome;
         this.vida = vidaInicial;
-        this.experiencia = 0; 
+        this.experiencia = 1; 
     }
 
-    public String getNome() {
-        return nome;
+
+    // 1. Atacar Básico Generico
+    public int atacar() {
+       
+        return 10; 
     }
 
-    public int getVida() {
-        return vida;
+    // 2.Receber Dano
+    public void receberDano(int danoRecebido) {
+        this.vida -= danoRecebido;
+
+        //A vida não pode ser menor que zero
+        if (this.vida < 0) {
+            this.vida = 0;
+        }
+
+        System.out.println(this.nome + " recebeu " + danoRecebido + " de dano. Vida restante: " + this.vida);
+        
+        if (this.vida == 0) {
+            System.out.println(this.nome + " caiu em combate!");
+        }
     }
 
-    public int getExperiencia() {
-        return experiencia;
-    }
+    // --- GETTERS ---
+    public String getNome() { return nome; }
+    public int getVida() { return vida; }
+    public int getExperiencia() { return experiencia; }
+    
+    // --- TESTE---
+    public static void main(String[] args) {
+        // Criando dois lutadores para testar
+        Combatente c1 = new Combatente("Lutador A", 100);
+        Combatente c2 = new Combatente("Lutador B", 100);
 
-    public void setVida(int vida) {
-        this.vida = vida;
-    }
-
-    public void setExperiencia(int experiencia) {
-        this.experiencia = experiencia;
+        // Simulando uma rodada simples
+        int danoDoAtaque = c1.atacar(); // C1 ataca
+        c2.receberDano(danoDoAtaque);   // C2 recebe o dano
     }
 }
