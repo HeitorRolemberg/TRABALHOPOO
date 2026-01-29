645e8935ba298aa8825d3145b04ffb2c35ae1bd7
Nicolle Santana <nsilvacademico@gmail.com>
2026-01-29 17:48:27 -0300

    Update Combatente.java

--- Elementos_do_Jogo/Combatente.java
+++ Elementos_do_Jogo/Combatente.java
@@ -1,33 +1,39 @@
 package Elementos_do_Jogo;
 
+import java.awt.Color;
+
 public abstract class Combatente {
     protected String nome;
-    private int vida;
+    protected int vida;
+    protected int vidaMaxima; // Usado para calcular o tamanho da barra verde
     protected int dano;
     protected int defesa;
+    protected Color cor; // Cor do personagem (Azul, Roxo, Verde)
     
     public Combatente(String nome, int vida, int dano, int defesa) {
         this.nome = nome;
         this.vida = vida;
+        this.vidaMaxima = vida;
         this.dano = dano;
         this.defesa = defesa;
     }
 
-    public abstract void atacar(Combatente oponente);
+    public abstract String atacar(Combatente oponente);
 
-    public void receberDano(int danoTotal){
+    public String receberDano(int danoTotal){
         if(this.vida > danoTotal){
             this.vida -= danoTotal;
+            return this.nome + " recebeu " + danoTotal + " de dano.";
         } else {
             this.vida = 0;
+            return this.nome + " CAIU EM BATALHA!";
         }
     }
 
-    public boolean estaVivo() {
-    return this.vida > 0;
-}
-
+    public boolean estaVivo() { return this.vida > 0; }
     public int getDefesa(){ return defesa; }
     public String getNome() { return nome; }
     public int getVida() { return vida; }
+    public int getVidaMaxima() { return vidaMaxima; }
+    public Color getCor() { return cor; }
 }
